75108ad4d0e02f9dbc84eb3480a8cfe820a9a0ef
Sibuchanda <sibuchanda457@gmail.com>
2025-12-21 21:35:26 +0530

    WebRTC offer-answer 2 way handshake is done

--- client/src/pages/Roompage.jsx
+++ client/src/pages/Roompage.jsx
@@ -1,27 +1,86 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import socket from "../config/socket";
+import RTC_CONFIG from "../config/RtcConfig";
 
 const RoomPage = () => {
   const { id: roomId } = useParams();
   const location = useLocation();
   const { email } = location.state || {};
 
+  const pcRef = useRef(null);
+  const localVideoRef = useRef(null);
+
   useEffect(() => {
     if (!email || !roomId) return;
 
-    socket.emit("join-room", {roomId,email});
-    socket.on("user-joined", ({email})=>{
-      console.log(`New User joined : ${email}`);
+    socket.emit("join-room", { roomId, email });
+
+    socket.on("user-joined", async ({ email: joinedEmail }) => {
+      pcRef.current = new RTCPeerConnection(RTC_CONFIG); // creating peer connection
+      const stream = await navigator.mediaDevices.getUserMedia({
+        // Getting media
+        video: true,
+        audio: true,
+      });
+
+      stream.getTracks().forEach((track) => {
+        pcRef.current.addTrack(track, stream);
+      });
+      localVideoRef.current.srcObject = stream;
+
+      const offer = await pcRef.current.createOffer(); // creating offer
+      await pcRef.current.setLocalDescription(offer);
+      socket.emit("call-user", { email: joinedEmail, offer }); //Seding offer request to the joined user
     });
 
+
+    socket.on("incoming-call", async ({ from, offer }) => {
+      //  console.log(`Incoming call from : ${from}  with offer : ${offer}`)
+      pcRef.current = new RTCPeerConnection(RTC_CONFIG);
+
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      stream.getTracks().forEach((track) => {
+        pcRef.current.addTrack(track, stream);
+      });
+      localVideoRef.current.srcObject = stream;
+      await pcRef.current.setRemoteDescription(offer); // Accept offer
+
+      const answer = await pcRef.current.createAnswer(); // Create answer
+      await pcRef.current.setLocalDescription(answer);
+
+      socket.emit("call-accepted", { // Send answer back
+        email: from,
+        answer,
+      });
+
+    });
+
+
+    socket.on("call-accepted", async({answer})=>{
+      await pcRef.current.setRemoteDescription(answer);
+    })
+
+    // Cleanup functions
+    return () => {
+      socket.off("user-joined");
+      socket.off("incoming-call");
+    };
   }, [roomId, email]);
 
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gray-100">
-      <h2 className="text-4xl font-bold">Room Page</h2>
-      <p>Room ID: {roomId}</p>
-      <p>Email: {email}</p>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h2 className="text-3xl font-bold mb-4">Room: {roomId}</h2>
+      <video
+        ref={localVideoRef}
+        autoPlay
+        muted
+        playsInline
+        className="w-72 border"
+      />
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-import express from 'express'
-import dotevn from 'dotenv';
+import express from "express";
+import dotevn from "dotenv";
 dotevn.config();
-import { createServer } from 'node:http';
-import { Server } from 'socket.io';
-import cors from 'cors';
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import cors from "cors";
 
 const PORT = process.env.PORT || 8001;
 const app = express();
@@ -16,22 +16,36 @@ const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URI,
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
+io.on("connection", (socket) => {
+  socket.on("join-room", ({ roomId, email }) => {
+    emailToSockerMapping.set(email, socket.id);
+    socketToEmailMapping.set(socket.id, email);
 
-io.on('connection',(socket)=>{
-     socket.on("join-room", ({roomId, email})=>{
-      emailToSockerMapping.set(email, socket.id);
-      socketToEmailMapping.set(socket.id, email);
-      
-      socket.join(roomId);
-      console.log(`User ${email} joined room no : ${roomId}`);
-      socket.broadcast.to(roomId).emit("user-joined", {email});
-     })
-})
+    socket.join(roomId);
+    console.log(`User ${email} joined room no : ${roomId}`);
+    socket.broadcast.to(roomId).emit("user-joined", { email });
+  });
 
-server.listen(PORT,()=>{
-    console.log(`Listening to port : ${PORT}`);
-})
+  socket.on("call-user", ({ email, offer }) => {
+    const fromEmail = socketToEmailMapping.get(socket.id); // Sender
+    const targetSocketId = emailToSockerMapping.get(email); // Receiver's SocketID
+
+    if (!targetSocketId) return;
+    socket.to(targetSocketId).emit("incoming-call", { from: fromEmail, offer });
+  });
+
+  socket.on("call-accepted", ({ email, answer }) => {
+    const targetSocketId = emailToSockerMapping.get(email);
+    if (!targetSocketId) return;
+
+    socket.to(targetSocketId).emit("call-accepted", { answer });
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening to port : ${PORT}`);
+});
