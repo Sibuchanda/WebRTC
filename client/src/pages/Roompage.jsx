766e909c62c0b5e201bf4b92554328e4aed87ea3
Sibuchanda <sibuchanda457@gmail.com>
2025-12-22 21:28:36 +0530

    Resolved On-page refress Remote peer video not showing error

--- client/src/pages/Roompage.jsx
+++ client/src/pages/Roompage.jsx
@@ -11,6 +11,7 @@ const RoomPage = () => {
   const pcRef = useRef(null);
   const localVideoRef = useRef(null);
   const remoteVideoRef = useRef(null);
+  const pendingIceCandidates = useRef([]);
 
   useEffect(() => {
     if (!email || !roomId) return;
@@ -21,7 +22,8 @@ const RoomPage = () => {
     socket.on("user-joined", async ({ email: joinedEmail }) => {
       pcRef.current = new RTCPeerConnection(RTC_CONFIG); // creating peer connection
 
-      pcRef.current.onicecandidate = (event) => { // Sending ICE candidates
+      pcRef.current.onicecandidate = (event) => {
+        // Sending ICE candidates
         if (event.candidate) {
           socket.emit("ice-candidate", {
             email: joinedEmail,
@@ -34,7 +36,8 @@ const RoomPage = () => {
         remoteVideoRef.current.srcObject = event.streams[0];
       };
 
-      const stream = await navigator.mediaDevices.getUserMedia({ // Getting media
+      const stream = await navigator.mediaDevices.getUserMedia({
+        // Getting media
         video: true,
         audio: true,
       });
@@ -46,6 +49,10 @@ const RoomPage = () => {
 
       const offer = await pcRef.current.createOffer(); // creating offer
       await pcRef.current.setLocalDescription(offer);
+      for (const c of pendingIceCandidates.current) {
+        await pcRef.current.addIceCandidate(c);
+      }
+      pendingIceCandidates.current = [];
       socket.emit("call-user", { email: joinedEmail, offer }); //Seding offer request to the joined user
     });
 
@@ -53,7 +60,8 @@ const RoomPage = () => {
     socket.on("incoming-call", async ({ from, offer }) => {
       pcRef.current = new RTCPeerConnection(RTC_CONFIG);
 
-      pcRef.current.onicecandidate = (event) => { // Sending ICE candidates
+      pcRef.current.onicecandidate = (event) => {
+        // Sending ICE candidates
         if (event.candidate) {
           socket.emit("ice-candidate", {
             email: from,
@@ -76,10 +84,16 @@ const RoomPage = () => {
       localVideoRef.current.srcObject = stream;
       await pcRef.current.setRemoteDescription(offer); // Accept offer
 
+      for (const c of pendingIceCandidates.current) {
+        await pcRef.current.addIceCandidate(c);
+      }
+      pendingIceCandidates.current = [];
+
       const answer = await pcRef.current.createAnswer(); // Create answer
       await pcRef.current.setLocalDescription(answer);
 
-      socket.emit("call-accepted", { // Send answer back
+      socket.emit("call-accepted", {
+        // Send answer back
         email: from,
         answer,
       });
@@ -92,8 +106,11 @@ const RoomPage = () => {
 
     // ================== ICE CANDIDATE RECEIVE =================
     socket.on("ice-candidate", async ({ candidate }) => {
-      if (candidate && pcRef.current) {
+      if (!pcRef.current || !candidate) return;
+      if (pcRef.current.remoteDescription) {
         await pcRef.current.addIceCandidate(candidate);
+      } else {
+        pendingIceCandidates.current.push(candidate);
       }
     });
 
@@ -106,25 +123,54 @@ const RoomPage = () => {
     };
   }, [roomId, email]);
 
-
-  
   return (
     <>
-      <h2 className="text-3xl font-bold mb-4">Room: {roomId}</h2>
-      <div className="min-h-screen flex gap-6 items-center justify-center bg-gray-100">
-        <video
-          ref={localVideoRef}
-          autoPlay
-          muted
-          playsInline
-          className="w-64 border"
-        />
-        <video
-          ref={remoteVideoRef}
-          autoPlay
-          playsInline
-          className="w-64 border"
-        />
+      <div className="h-screen w-screen overflow-hidden flex flex-col bg-gray-100">
+        <header className="shrink-0 py-3 text-center">
+          <h2 className="text-lg sm:text-xl font-semibold">
+            Room ID : {roomId}
+          </h2>
+          <div className="flex items-center gap-1 bg-black w-fit p-2 rounded-full ml-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full animate-pulse" />
+            <span className="text-slate-300 text-sm font-medium tracking-wide">
+              LIVE
+            </span>
+          </div>
+        </header>
+
+        <main className="relative flex-1 flex items-center justify-center overflow-hidden">
+          {/* Remote Video */}
+          <video
+            ref={remoteVideoRef}
+            autoPlay
+            playsInline
+            className="
+            w-full
+            h-full
+            object-contain
+            max-w-[1200px]
+            rounded-md
+          "
+          />
+          {/* Local Video  */}
+          <video
+            ref={localVideoRef}
+            autoPlay
+            muted
+            playsInline
+            className="
+            absolute
+            bottom-3
+            right-3
+            w-28
+            sm:w-36
+            md:w-44
+            aspect-video
+            rounded-md
+            shadow-md
+          "
+          />
+        </main>
       </div>
     </>
   );
